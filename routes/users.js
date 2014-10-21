a4b5140487d82832898dd0a0c86e2bd5154f9f0d
Yaohui Ye <yeyh10@gmail.com>
2014-10-21 13:04:27 -0700

    added some tests and changed user

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,19 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+router.post('/create_user', function(req, res) {
+  var nvalidU = data.checkUser(req.body.user);
+  var nvalidP = data.checkPwd(req.body.password);
+  if (nvalidP) return res.json({'errCode': nvalidP});
+  if (nvalidU) return res.json({'errCode': nvalidU});
+
+  data.add(req.body.username, req.body.password, function(result) {
+    return res.json(result);
+  })
+});
+
 router.post('/login', function(req, res) {
-  data.login(req.body.user, req.body.password, function(result) {
+  data.login(req.body.username, req.body.password, function(result) {
     return res.json(result);
   });
 });
@@ -26,17 +37,6 @@ router.post('/search_users_by_name', function(req, res) {
   data.getUserByName(req.body.user_name, function(result) {
     return res.json(result);
   });
-});
-
-router.post('/create_user', function(req, res) {
-  var nvalidU = data.checkUser(req.body.user);
-  var nvalidP = data.checkPwd(req.body.password);
-  if (nvalidP) return res.json({'errCode': nvalidP});
-  if (nvalidU) return res.json({'errCode': nvalidU});
-
-  data.add(req.body.user, req.body.password, function(result) {
-    return res.json(result);
-  })
 });
 
 module.exports = router;
